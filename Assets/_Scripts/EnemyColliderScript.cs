3883fd77f74ba392b393ecbf17b8cea65a6d921a
Jafke104 <eddevera@uci.edu>
2018-10-19 22:07:15 -0700

    WIP Collider

---
+++ Assets/_Scripts/EnemyColliderScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//WIP WIP WIP WIP WIP WIP
+public class EnemyColliderScript : MonoBehaviour {
+
+    LayerMask EnemyLayerMask;
+    Transform myTransform;
+
+    void Awake()
+    {
+        EnemyLayerMask = (1 << 8) | (1 << 9);
+        myTransform = transform;
+    }
+    // Use this for initialization
+    void Start () {
+        Physics2D.SetLayerCollisionMask(8, EnemyLayerMask);
+        Physics2D.IgnoreCollision(myTransform.GetComponent<CircleCollider2D>,);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
