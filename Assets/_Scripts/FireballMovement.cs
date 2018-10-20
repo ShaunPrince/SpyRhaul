2d14cbc0943f2d133bf04163c948df7d22d63b04
sowrd299 <ryan.ward46@gmail.com>
2018-10-20 10:08:54 -0700

    adds balls of fire

---
+++ Assets/_Scripts/FireballMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireballMovement : MonoBehaviour {
+
+	public float speed = 10;
+
+	private Rigidbody2D rb;
+
+	//needs to be awake so comes before fire
+	void Awake(){
+		rb = gameObject.GetComponent<Rigidbody2D>();
+		Debug.Log(rb.ToString());
+	}
+
+	//returns if fired
+	public void Fire(){
+		rb.AddForce(transform.up * speed);
+	}
+	
+
+}
