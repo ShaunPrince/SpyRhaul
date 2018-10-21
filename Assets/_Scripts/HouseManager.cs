f93e0f8b2f2c980b5b446da7b643b7cf388acb82
Jafke104 <eddevera@uci.edu>
2018-10-21 10:17:32 -0700

    fix amInCloud

---
+++ Assets/_Scripts/HouseManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HouseManager : MonoBehaviour {
+
+    public GameObject building;
+    public GameObject housePrefab;
+    // Use this for initialization
+    void Start () {
+        for (int i = 0; i < building.transform.childCount; i++){
+
+            Instantiate(housePrefab, building.transform.GetChild(i).transform.position, building.transform.GetChild(i).transform.rotation, building.transform.GetChild(i));
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
