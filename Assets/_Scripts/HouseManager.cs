14d58298b964a05c0947f1e08dbdddcb0566ac8f
Jafke104 <eddevera@uci.edu>
2018-10-21 10:39:52 -0700

    House Triggers work

--- Assets/_Scripts/HouseManager.cs
+++ Assets/_Scripts/HouseManager.cs
@@ -4,18 +4,23 @@ using UnityEngine;
 
 public class HouseManager : MonoBehaviour {
 
-    public GameObject building;
-    public GameObject housePrefab;
     // Use this for initialization
-    void Start () {
-        for (int i = 0; i < building.transform.childCount; i++){
-
-            Instantiate(housePrefab, building.transform.GetChild(i).transform.position, building.transform.GetChild(i).transform.rotation, building.transform.GetChild(i));
+    private void Awake()
+    {
+        for (int i = 0; i < gameObject.GetComponents<Collider2D>().Length; i++)
+        {
+            gameObject.GetComponents<Collider2D>()[i].isTrigger = true;
         }
+    }
+
+    void Start ()
+    {
+
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+    {
 		
 	}
 }
